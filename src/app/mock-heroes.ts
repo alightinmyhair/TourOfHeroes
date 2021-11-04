ff18be567060433e2ae19386143d35d377301daf
Rachelle Kuhl <alightinmyhair@gmail.com>
2021-11-03 19:58:41 -0500

    Created an array of hero objects and added styling to hero list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Dr. Nice' },
+    {id: 12, name: 'Pickles' },
+    {id: 13, name: 'Wiggler' },
+    {id: 14, name: 'Tornado' },
+    {id: 15, name: 'Climate' },
+    {id: 16, name: 'Peace' },
+    {id: 17, name: 'Brainss' },
+    {id: 18, name: 'Dr. Evil' },
+    {id: 19, name: 'Yoghurt' },
+    {id: 20, name: 'StrongWoman'}
+]
